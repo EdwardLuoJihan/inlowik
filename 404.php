a638e8ff4f31ab5d86f39e6d2699e6c45a34a9ea
EdwardLuoJihan <62911139+EdwardLuoJihan@users.noreply.github.com>
2022-01-25 17:44:03 -0700

    stuff

--- 404.php
+++ 404.php
@@ -1,5 +1,7 @@
 <link rel="stylesheet" href="styles/404.css">
 
+<title>404 Page Not Found | Inlowik</title>
+
 <body>
     <img src="images/404.png" alt="" height="70%">
     <h1>The Page You're Looking For Doesn't Exist!</h1>
